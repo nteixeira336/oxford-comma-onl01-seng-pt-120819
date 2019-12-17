72e6924fa67d37cd044de72043a216331ad72aa4
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 23:43:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.length==1 
+  array[0]
 
+  elsif array.length ==2 
+   return array.join(" and ")
+    
+  elsif array.length>2   
+   array[-1].insert(0, "and ")
+  end 
+  array.join(", ") 
+ 
 end
+
+
