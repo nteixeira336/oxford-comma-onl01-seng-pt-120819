f84019dc03b407340f85d3d242764841d1df46c7
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-17 23:21:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-  if array.length==1 then
+  if array.length==1 
   array[0]
 
-  elsif array.length ==2 then 
+  elsif array.length ==2 
     array.join(" and ")
     
-  elsif array.length==3 then   
+  elsif array.length==3   
     array.join("," " and ")
   end 
    
